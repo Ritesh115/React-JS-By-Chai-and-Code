68186cfe0f2837a89a4378d9079d11c48bbfd8c5
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2024-10-13 19:49:34 +0530

    commit

--- 01basicreact/src/main.jsx
+++ 01basicreact/src/main.jsx
@@ -1,5 +1,5 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
@@ -7,3 +7,11 @@ createRoot(document.getElementById("root")).render(
     <App />
   </StrictMode>
 );
+
+//OPTIONAL
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
