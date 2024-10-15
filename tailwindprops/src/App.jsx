076221afa43dd26fba7133b9818b17d5c72013d1
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2024-10-15 12:08:12 +0000

    commit

---
+++ tailwindprops/src/App.jsx
@@ -0,0 +1,19 @@
+import Card from "../components/Card";
+
+function App() {
+  return (
+    <>
+      <h1
+        className="bg-green-400 text-black 
+   p-4 rounded-xl mb-4"
+      >
+        Tailwind
+      </h1>
+
+      <Card />
+      <Card />
+    </>
+  );
+}
+
+export default App;
