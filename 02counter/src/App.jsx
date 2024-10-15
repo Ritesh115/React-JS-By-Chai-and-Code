68ba44cb03e1ac154ea5719ddd421ae8ac6e6d5f
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2024-10-15 06:09:18 +0000

    commit

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,16 +1,30 @@
+import { useState } from "react";
+
 function App() {
-  let counter = 15;
-  document.getElementById("button1").addEventListener("click", () => {
-    console.log("clicked", Math.random());
+  // let counter = 15;
+  //ysestate is called here
+  let [counter, setCounter] = useState(15);
+
+  const addValue = () => {
+    console.log("value", counter);
     counter += 1;
-  });
+    setCounter(counter);
+  };
+
+  //2.
+  const removeValue = () => {
+    console.log("value", counter);
+    counter -= 1;
+    setCounter(counter);
+  };
 
   return (
     <>
       <h1>Welcome to count game</h1>
       <h2>counter value : {counter}</h2>
-      <button id="button1">Add value</button> <br />
-      <button id="button2">Remove value</button>
+      <button onClick={addValue}>Add value</button> <br />
+      <button onClick={removeValue}>Remove value</button>
+      <p>value is {counter}</p>
     </>
   );
 }
