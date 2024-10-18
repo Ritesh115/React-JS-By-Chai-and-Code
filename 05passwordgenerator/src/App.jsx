b14bf6aac2eb686c3833ae775ca423e025e7b13e
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2024-10-18 07:02:08 +0000

    commit

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -14,18 +14,49 @@ function App() {
     if (numberAllowed) str += "0123456789";
     if (charAllowed) str += "~!@#$%^&*()+-*/";
 
-    for (let i = 0; i <= Array.length; i++) {
-      const 
+    for (let i = 0; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass = pass + char;
     }
+
+    setpassword(pass);
   }, [length, numberAllowed, charAllowed, setpassword]);
   return (
     <>
-      <h1
-        className="text-4xl text-center 
-   text-white"
+      <div
+        className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 
+       py-8 my-8 text-orange-500 bg-gray-700"
       >
-        Password generator
-      </h1>
+        <h1 className="text-white text-center">Password generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={Password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly
+          />
+          <button className="outline-none  bg-blue-700 text-white px-3 py-0.5 shrink-0">
+            copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex item-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setlength(e.target.value);
+              }}
+            />
+            <label>Length: {length}</label>
+          </div>
+          <div></div>
+        </div>
+      </div>
     </>
   );
 }
