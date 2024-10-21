a0a90da6e7578ccc3220c1fb4bcf33151a2f1399
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2024-10-21 11:22:05 +0000

    commit

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import "./App.css";
+import InputBox from "./components/InputBox.jsx";
 
 function App() {
   return (
     <>
       <h1>hello welcome</h1>
+      <InputBox />
     </>
   );
 }
