217c68c77928c4e0c021af0b7d50e37fd76d2a12
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2024-10-26 14:59:04 +0000

    commit

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-blue-400 p-4">React Router</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 07reactRouter/src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <header>
+        <div id="">
+          <p>Yourlogo</p>
+        </div>
+        <div id="">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Github</li>
+          </ul>
+        </div>
+        <div id=""></div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
