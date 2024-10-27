0548ef72d70bb3256b39f9cfcef8eb036ab950bd
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2024-10-27 13:06:30 +0000

    commit

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
-
+import Header from "./components/Header/Header";
 function App() {
   return (
     <>
       <h1 className="bg-blue-400 p-4">React Router</h1>
+      <Header />
     </>
   );
 }
