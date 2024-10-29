24727fb7e701aa017f164c56ff1307036c9b7b10
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2024-10-29 07:43:11 +0000

    commit

--- reactRouterPractise/src/App.jsx
+++ reactRouterPractise/src/App.jsx
@@ -1,9 +1,28 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Dashboard from "./components/Dashboard";
 
-const router = createBrowserRouter([{ path: "/" }, {}, {}]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    //jab ye wala path / aap choose karanga tab konsa page app render karwana chata ho ..dal do element me
+    //path kon sa hoga and element konsa load karwana hai.
+    element: <Home />,
+  },
+  { path: "/about", element: <About /> },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+]);
 
 function App() {
-  return <h1>Hello jee</h1>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
